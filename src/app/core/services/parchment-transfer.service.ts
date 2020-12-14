7d497dd1a59f032755af3cfaea1353199eb49278
Rukundo Joseph <imagination081@gmail.com>
2020-12-14 02:37:49 +0200

    save

---
+++ src/app/core/services/parchment-transfer.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {isNullOrUndefined} from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParchmentTransferService {
+
+  constructor() {
+  }
+
+  resetCart() {
+    localStorage.removeItem('parchmentTransferCart');
+  }
+
+  getCart() {
+    if (!isNullOrUndefined(localStorage.getItem('parchmentTransferCart'))) {
+      return JSON.parse(localStorage.getItem('parchmentTransferCart'));
+    } else {
+      return [];
+    }
+  }
+
+  setCart(value: any) {
+    localStorage.setItem('parchmentTransferCart', JSON.stringify(value));
+  }
+}
