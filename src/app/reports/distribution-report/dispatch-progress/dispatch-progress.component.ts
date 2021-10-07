3840eb606b4758563424aa41a2152befa8c66ecf
mugenga <rockyyves@gmail.com>
2021-10-07 16:04:12 +0200

    Quick fixes on BI Report

--- src/app/reports/distribution-report/dispatch-progress/dispatch-progress.component.ts
+++ src/app/reports/distribution-report/dispatch-progress/dispatch-progress.component.ts
@@ -1,26 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {Subject} from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Subject } from "rxjs";
 import {
   AuthenticationService,
-  AuthorisationService, ExcelServicesService, InputDistributionService, LocationService,
+  AuthorisationService,
+  ExcelServicesService,
+  InputDistributionService,
+  LocationService,
   OrganisationService,
   OrganisationTypeService,
-  SiteService
-} from '../../../core/services';
-import {Router} from '@angular/router';
-import {HelperService} from '../../../core/helpers';
-import {isUndefined} from 'util';
-import {BasicComponent} from '../../../core/library';
+  SiteService,
+} from "../../../core/services";
+import { Router } from "@angular/router";
+import { HelperService } from "../../../core/helpers";
+import { isUndefined } from "util";
+import { BasicComponent } from "../../../core/library";
 
 @Component({
-  selector: 'app-dispatch-progress',
-  templateUrl: './dispatch-progress.component.html',
-  styleUrls: ['./dispatch-progress.component.css']
+  selector: "app-dispatch-progress",
+  templateUrl: "./dispatch-progress.component.html",
+  styleUrls: ["./dispatch-progress.component.css"],
 })
-export class DispatchProgressComponent extends BasicComponent implements OnInit {
-
-  title = 'Distribution progress';
+export class DispatchProgressComponent
+  extends BasicComponent
+  implements OnInit
+{
+  title = "Distribution progress";
   checkProgressForm: FormGroup;
   errors: any;
   loading = false;
@@ -40,38 +45,45 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
   siteTotalAllocated = 0;
   siteTotalDispatched = 0;
   graph = {
-    type: 'ColumnChart',
+    type: "ColumnChart",
     data: [],
     options: {
-      colors: ['#367fa9', '#f0a732']
+      colors: ["#367fa9", "#f0a732"],
     },
-    columnNames: ['Farmers', 'allocated', 'dispatched'],
+    columnNames: ["Farmers", "allocated", "dispatched"],
     width: 1050,
-    height: 450
+    height: 450,
   };
 
-  constructor(private formBuilder: FormBuilder, private siteService: SiteService,
-              private authorisationService: AuthorisationService,
-              private authenticationService: AuthenticationService,
-              private excelService: ExcelServicesService,
-              private router: Router, private organisationService: OrganisationService,
-              private helper: HelperService, private organisationTypeService: OrganisationTypeService,
-              private locationService: LocationService, private inputDistributionService: InputDistributionService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private siteService: SiteService,
+    private authorisationService: AuthorisationService,
+    private authenticationService: AuthenticationService,
+    private excelService: ExcelServicesService,
+    private router: Router,
+    private organisationService: OrganisationService,
+    private helper: HelperService,
+    private organisationTypeService: OrganisationTypeService,
+    private locationService: LocationService,
+    private inputDistributionService: InputDistributionService
+  ) {
     super();
   }
 
   ngOnInit() {
-    this.isCurrentUserDCC = this.authorisationService.isDistrictCashCropOfficer();
+    this.isCurrentUserDCC =
+      this.authorisationService.isDistrictCashCropOfficer();
     this.checkProgressForm = this.formBuilder.group({
       location: this.formBuilder.group({
-        prov_id: [''],
-        dist_id: [''],
+        prov_id: [""],
+        dist_id: [""],
         siteId: [],
       }),
     });
     this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 25
+      pagingType: "full_numbers",
+      pageLength: 25,
     };
     this.initial();
     this.onFilterProgress();
@@ -83,26 +95,29 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
       this.showReport = false;
       const request = JSON.parse(JSON.stringify(this.checkProgressForm.value));
 
-      if (request.location.prov_id === '' && searchBy === 'province') {
+      if (request.location.prov_id === "" && searchBy === "province") {
         delete request.location;
-        request['location'.toString()] = {
-          searchBy: 'all provinces'
+        request["location".toString()] = {
+          searchBy: "all provinces",
         };
       } else {
-        request.location['searchBy'.toString()] = searchBy;
+        request.location["searchBy".toString()] = searchBy;
         this.helper.cleanObject(request.location);
       }
-      if (searchBy !== 'site') {
+      if (searchBy !== "site") {
         delete request.siteId;
       }
-      this.inputDistributionService.getDispatchProgress(request).subscribe((data) => {
-          if ((data.content.length !== 0) && (data.content)) {
+      this.inputDistributionService.getDispatchProgress(request).subscribe(
+        (data) => {
+          if (data.content.length !== 0 && data.content) {
             const reports = [];
-            if (request.location['searchBy'.toString()] === 'all provinces') {
+            if (request.location["searchBy".toString()] === "all provinces") {
               data.content.map((prov) => {
                 const temp = [prov.name, 0, 0];
                 if (!isUndefined(location)) {
-                  temp[1] = prov.totalFertilizerAllocated ? prov.totalFertilizerAllocated : 0;
+                  temp[1] = prov.totalFertilizerAllocated
+                    ? prov.totalFertilizerAllocated
+                    : 0;
                   temp[2] = prov.totalDispatched ? prov.totalDispatched : 0;
                 }
                 reports.push(temp);
@@ -111,29 +126,37 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
               this.showReport = true;
               this.showData = false;
               this.graph.data = reports;
-
-            } else if (request.location['searchBy'.toString()] === 'province') {
-              this.locationService.getDistricts(request.location.prov_id).subscribe((districts) => {
-                districts.map((dist) => {
-                  const location = data.content.find(obj => obj._id === dist._id);
-                  const temp = [dist.name, 0, 0];
-                  if (!isUndefined(location)) {
-                    temp[1] = location.totalFertilizerAllocated ? location.totalFertilizerAllocated : 0;
-                    temp[2] = location.totalDispatched ? location.totalDispatched : 0;
-                  }
-                  reports.push(temp);
+            } else if (request.location["searchBy".toString()] === "province") {
+              this.locationService
+                .getDistricts(request.location.prov_id)
+                .subscribe((districts) => {
+                  districts.map((dist) => {
+                    const location = data.content.find(
+                      (obj) => obj._id === dist._id
+                    );
+                    const temp = [dist.name, 0, 0];
+                    if (!isUndefined(location)) {
+                      temp[1] = location.totalFertilizerAllocated
+                        ? location.totalFertilizerAllocated
+                        : 0;
+                      temp[2] = location.totalDispatched
+                        ? location.totalDispatched
+                        : 0;
+                    }
+                    reports.push(temp);
+                  });
+                  this.loading = false;
+                  this.showReport = true;
+                  this.showData = false;
+                  this.graph.data = reports;
                 });
-                this.loading = false;
-                this.showReport = true;
-                this.showData = false;
-                this.graph.data = reports;
-              });
-
-            } else if (request.location['searchBy'.toString()] === 'district') {
+            } else if (request.location["searchBy".toString()] === "district") {
               data.content.map((site) => {
                 const temp = [site.name, 0, 0];
                 if (!isUndefined(location)) {
-                  temp[1] = site.totalFertilizerAllocated ? site.totalFertilizerAllocated : 0;
+                  temp[1] = site.totalFertilizerAllocated
+                    ? site.totalFertilizerAllocated
+                    : 0;
                   temp[2] = site.totalDispatched ? site.totalDispatched : 0;
                 }
                 reports.push(temp);
@@ -152,7 +175,7 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
             this.clear();
             this.printable = reports;
           } else {
-            this.setMessage('Sorry no data found for this location');
+            this.setMessage("Sorry no data found for this location");
             this.loading = false;
           }
         },
@@ -164,28 +187,32 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
           } else {
             this.setError(err.errors);
           }
-        });
+        }
+      );
     } else {
-      this.setError(this.helper.getFormValidationErrors(this.checkProgressForm));
+      this.setError(
+        this.helper.getFormValidationErrors(this.checkProgressForm)
+      );
     }
   }
 
   onFilterProgress() {
-    this.checkProgressForm.controls.location.get('prov_id'.toString()).valueChanges.subscribe(
-      (value) => {
-        if (value !== '') {
+    this.checkProgressForm.controls.location
+      .get("prov_id".toString())
+      .valueChanges.subscribe((value) => {
+        if (value !== "") {
           this.locationService.getDistricts(value).subscribe((data) => {
             this.districts = data;
           });
         }
-      }
-    );
-    this.checkProgressForm.controls.location.get('dist_id'.toString()).valueChanges.subscribe(
-      (value) => {
-        if (value !== '') {
+      });
+    this.checkProgressForm.controls.location
+      .get("dist_id".toString())
+      .valueChanges.subscribe((value) => {
+        if (value !== "") {
           const body = {
-            searchBy: 'district',
-            dist_id: value
+            searchBy: "district",
+            dist_id: value,
           };
           this.siteService.all(body).subscribe((data) => {
             this.sites = data.content;
@@ -194,11 +221,11 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
         } else {
           this.distId = false;
         }
-      }
-    );
-    this.checkProgressForm.controls.location.get('siteId').valueChanges.subscribe(
-      (value) => {
-        this.siteId = value !== '';
+      });
+    this.checkProgressForm.controls.location
+      .get("siteId")
+      .valueChanges.subscribe((value) => {
+        this.siteId = value !== "";
       });
   }
 
@@ -206,18 +233,30 @@ export class DispatchProgressComponent extends BasicComponent implements OnInit 
     this.locationService.getProvinces().subscribe((data) => {
       this.provinces = data;
       if (this.isCurrentUserDCC) {
-        this.checkProgressForm.controls.location.get('prov_id'.toString())
-          .patchValue(this.authenticationService.getCurrentUser().info.location.prov_id);
-        this.checkProgressForm.controls.location.get('dist_id'.toString())
-          .patchValue(this.authenticationService.getCurrentUser().info.location.dist_id);
+        this.checkProgressForm.controls.location
+          .get("prov_id".toString())
+          .patchValue(
+            this.authenticationService.getCurrentUser().info.location.prov_id
+          );
+        this.checkProgressForm.controls.location
+          .get("dist_id".toString())
+          .patchValue(
+            this.authenticationService.getCurrentUser().info.location.dist_id
+          );
       }
     });
   }
   exportReport() {
-    this.excelService.exportAsExcelFile(this.printable, 'report');
+    this.excelService.exportAsExcelFile(this.printable, "report");
   }
 
-  detailedReport() {
-    this.excelService.exportDetailedExcel();
+  async detailedReport() {
+    this.loading = true;
+    const status = await this.excelService.exportDetailedExcel();
+    if (!status) {
+      this.errors(["Error Exporting File"]);
+    }
+
+    this.loading = !status;
   }
 }
