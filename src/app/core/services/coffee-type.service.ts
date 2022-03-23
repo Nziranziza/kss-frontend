d8f0253ae71ec0308bf7f2d038083d839f53eae2
mugenga <rockyyves@gmail.com>
2022-03-23 15:09:16 +0200

    edit coffee types

--- src/app/core/services/coffee-type.service.ts
+++ src/app/core/services/coffee-type.service.ts
@@ -13,6 +13,18 @@ export class CoffeeTypeService {
     return this.apiService.get('/coffeetype');
   }
 
+  one(id: string): Observable<any> {
+    return this.apiService.get('/coffeetype/' + id);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.apiService.delete('/coffeetype/' + id);
+  }
+
+  update(body: any): Observable<any> {
+    return this.apiService.put('/coffeetype', body);
+  }
+
   save(body: any) {
     return this.apiService.post('/coffeetype', body);
   }
--- src/app/settings/coffee-type/coffee-type.component.ts
+++ src/app/settings/coffee-type/coffee-type.component.ts
@@ -1,16 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {CoffeeTypeService} from '../../core/services';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { CoffeeTypeService } from "../../core/services";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { DeleteTypeModal } from './coffee-type-delete-modal/coffee-type-delete-modal-component';
 
 @Component({
-  selector: 'app-coffee-type',
-  templateUrl: './coffee-type.component.html',
-  styleUrls: ['./coffee-type.component.css']
+  selector: "app-coffee-type",
+  templateUrl: "./coffee-type.component.html",
+  styleUrls: ["./coffee-type.component.css"],
 })
 export class CoffeeTypeComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private coffeeTypeService: CoffeeTypeService) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private modal: NgbModal,
+    private coffeeTypeService: CoffeeTypeService
+  ) {}
 
   message: string;
   coffeeTypes: any;
@@ -20,11 +24,18 @@ export class CoffeeTypeComponent implements OnInit {
   }
 
   getAllCoffeeTypes(): void {
-    this.coffeeTypeService.all().subscribe(data => {
+    this.coffeeTypeService.all().subscribe((data) => {
       if (data && data.content) {
         this.coffeeTypes = data.content;
       }
     });
   }
 
+  openDeleteModal(category: any) {
+    const modalRef = this.modal.open(DeleteTypeModal);
+    modalRef.componentInstance.category = category;
+    modalRef.result.finally(() => {
+      this.getAllCoffeeTypes();
+    });
+  }
 }
